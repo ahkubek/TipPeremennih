6920288e8eeac338550c81c9971d4931f2761052
ahkubek <120653318+ahkubek@users.noreply.github.com>
2022-12-26 11:44:33 +0300

    Задача2

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float  a = 27.12f;
+        int b = 569;
+        short c = -159;
+        int d = 27897;
+        byte e = 67;
+        long g= 987_678_965_549L;
+        double f = 2.786d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(f);
+    }
+}
